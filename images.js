8a79643ca8aeb6c91fb89b3f6dc462c8213aec41
tyler <tmillner17@gmail.com>
2019-09-28 09:41:58 -0600

    first commit

---
+++ images.js
@@ -0,0 +1,29 @@
+import hognose from 'C:\Users\Tyler\Desktop\Homework\React-clicky-game\src\images\29833441881_92f586b00b_b.jpg';
+import rattlesnake from 'C:\Users\Tyler\Desktop\Homework\React-clicky-game\src\images\635852868807003634-Snake.jpg';
+import retic from 'C:\Users\Tyler\Desktop\Homework\React-clicky-game\src\images\Anaconda snake photo.jpg';
+import viper from 'C:\Users\Tyler\Desktop\Homework\React-clicky-game\src\images\blue-viper-venomous-poisonous-snake-Venomous-Snakes-ss-Featured.jpg';
+import diamondback from 'C:\Users\Tyler\Desktop\Homework\React-clicky-game\src\images\s-l1000.jpg';
+import brown from 'C:\Users\Tyler\Desktop\Homework\React-clicky-game\src\images\Eastern-Brown-Snake-Background-.jpg';
+import eyelash from 'C:\Users\Tyler\Desktop\Homework\React-clicky-game\src\images\snake_cover-1500x1000.jpeg';
+import cornsnake from 'C:\Users\Tyler\Desktop\Homework\React-clicky-game\src\images\snake-pic-1728x1080-b688dd6.png';
+import yellow from 'C:\Users\Tyler\Desktop\Homework\React-clicky-game\src\images\th (1).jpg';
+import cobra from 'C:\Users\Tyler\Desktop\Homework\React-clicky-game\src\images\th.jpg';
+import emerald from 'C:\Users\Tyler\Desktop\Homework\React-clicky-game\src\images\Snake HD Funny Wallpapers 15.jpg';
+import burmese from 'C:\Users\Tyler\Desktop\Homework\React-clicky-game\src\images\yellow-snake-76946d.jpg';
+
+const images = [
+  hognose,
+  rattlesnake,
+  retic,
+  viper,
+  diamondback,
+  brown,
+  eyelash,
+  cornsnake,
+  yellow,
+  cobra,
+  emerald,
+  burmese
+];
+
+export default images;
